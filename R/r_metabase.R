577165e33abf67456764996eb92da745124d9b52
Balázs Klein <Balazs.Klein@gmail.com>
2021-11-04 12:32:11 +0100

    correct medeen
    check if no parameters are supplied

--- R/r_metabase.R
+++ R/r_metabase.R
@@ -16,24 +16,22 @@ metabase_init <- function (base_url, username, password = NULL) {
     prompt <- paste("Enter Metabase Password for ", username)
     password <- .rs.askForPassword(prompt)
   }
-
+  
   creds<-list(
     username = username,
     password = password
   )
-  credsAsJSON <- jsonlite::toJSON(creds, auto_unbox=TRUE)
-
-  req <- httr::POST(mb_url(base_url, '/api/session'),
-                    httr::add_headers(
-                      "Content-Type" = "application/json"
-                    ),
-                    body = credsAsJSON
-  )
-
+  
+  base_url<-sub("/$","",base_url) #Remove final slash if present metabase can't handle //
+  
+  req <- httr::POST(mb_url(base_url, '/api/session'),body = creds,encode="json")
+  
   if (req$status_code >= 200 && req$status_code < 300) {
-    jsonAuthResponse <- httr::content(req, "text")
+    jsonAuthResponse <- httr::content(req, "text", encoding="UTF-8")
+    #cat(jsonAuthResponse)
+    #return(list(req=req,resp=jsonAuthResponse))
     mb_session_token <- toString(jsonlite::fromJSON(jsonAuthResponse))
-
+    
     # make sure session is legit
     mb_session <- list(
       base_url = base_url,
@@ -64,21 +62,23 @@ metabase_fetch_question <- function(metabase_session, id, params = list(), tmpdi
   mb_verify_session(metabase_session)
   
   #Example: 'parameters=[{"type":"category","target":["variable",["template-tag","project_id"]],"value":"2559"}]'
-
+  
   param_names<-names(params)
   param_vals<-unlist(params, use.names=FALSE)
   data=list()
-  for (i in 1:length(param_names)) {
-    data[[i]]=list("type"="category","target"=list("variable",list("template-tag",param_names[i])),"value"=param_vals[i])
+  if (length(param_names)>0){
+    for (i in 1:length(param_names)) {
+      data[[i]]=list("type"="category","target"=list("variable",list("template-tag",param_names[i])),"value"=param_vals[i])
+    }
+    data<-list("parameters"=jsonlite::toJSON(data, auto_unbox = TRUE))
   }
-  data<-list("parameters"=jsonlite::toJSON(data, auto_unbox = TRUE))
-
+  
   query_url_part = paste0('/api/card/', id, '/query/json', "")
   req <- httr::POST(mb_url(metabase_session$base_url, query_url_part),
                     httr::add_headers("X-Metabase-Session" = metabase_session$token, "Content-Type"="application/x-www-form-urlencoded"),
                     body=data, encode="form")#, httr::verbose())
   mb_req_error_processor(req)
-
+  
   questionJSON <- httr::content(req,"text")
   if (tmpdir!=FALSE){
     tfile<-tempfile(tmpdir=tmpdir,fileext=".json")
